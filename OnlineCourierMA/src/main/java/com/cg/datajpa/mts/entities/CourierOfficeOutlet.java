000677fe4cccdc0f437a67f4a8340ecb56b52f8f
oo7pa <oo7pa@DESKTOP-ATILLA>
2021-04-16 15:39:58 +0530

    update office outlet

--- OnlineCourierMA/src/main/java/com/cg/datajpa/mts/entities/CourierOfficeOutlet.java
+++ OnlineCourierMA/src/main/java/com/cg/datajpa/mts/entities/CourierOfficeOutlet.java
@@ -1,4 +1,4 @@
-package com.cg.mts.entities;
+package com.cg.datajpa.mts.entities;
 
 import java.time.LocalTime;
 import java.util.List;
@@ -9,5 +9,55 @@ public class CourierOfficeOutlet {
 	private LocalTime openingTime;
 	private LocalTime closingTime;	
 	private List<OfficeStaffMember> staffmembers;
+	public CourierOfficeOutlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CourierOfficeOutlet(int officeid, Address address, LocalTime openingTime, LocalTime closingTime,
+			List<OfficeStaffMember> staffmembers) {
+		super();
+		this.officeid = officeid;
+		this.address = address;
+		this.openingTime = openingTime;
+		this.closingTime = closingTime;
+		this.staffmembers = staffmembers;
+	}
+	public int getOfficeid() {
+		return officeid;
+	}
+	public void setOfficeid(int officeid) {
+		this.officeid = officeid;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public LocalTime getOpeningTime() {
+		return openingTime;
+	}
+	public void setOpeningTime(LocalTime openingTime) {
+		this.openingTime = openingTime;
+	}
+	public LocalTime getClosingTime() {
+		return closingTime;
+	}
+	public void setClosingTime(LocalTime closingTime) {
+		this.closingTime = closingTime;
+	}
+	public List<OfficeStaffMember> getStaffmembers() {
+		return staffmembers;
+	}
+	public void setStaffmembers(List<OfficeStaffMember> staffmembers) {
+		this.staffmembers = staffmembers;
+	}
+	@Override
+	public String toString() {
+		return "CourierOfficeOutlet [officeid=" + officeid + ", address=" + address + ", openingTime=" + openingTime
+				+ ", closingTime=" + closingTime + ", staffmembers=" + staffmembers + "]";
+	}
+	
+	
 	
 }
