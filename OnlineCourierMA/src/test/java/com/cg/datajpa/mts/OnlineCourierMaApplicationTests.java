7328e5f00e2ffa98aa0e1388c5db2f65aa33df67
oo7pa <oo7pa@DESKTOP-ATILLA>
2021-04-16 11:57:46 +0530

    basic skeleton

---
+++ OnlineCourierMA/src/test/java/com/cg/datajpa/mts/OnlineCourierMaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.datajpa.mts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourierMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
