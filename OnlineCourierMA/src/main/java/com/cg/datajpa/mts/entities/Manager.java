3cd090e8f94624f250fdeaa2d8ea52a463bd3c5b
VenkateshMurthy912 <82637569+VenkateshMurthy912@users.noreply.github.com>
2021-04-16 12:27:40 +0530

    Update Manager.java

--- OnlineCourierMA/src/main/java/com/cg/datajpa/mts/entities/Manager.java
+++ OnlineCourierMA/src/main/java/com/cg/datajpa/mts/entities/Manager.java
@@ -5,4 +5,25 @@ import java.util.List;
 public class Manager extends OfficeStaffMember {
 
 	private List<OfficeStaffMember> reportingstaffmembers;
+	public Manager() {
+		
+	}
+
+	public Manager(List<OfficeStaffMember> reportingstaffmembers) {
+		
+		this.reportingstaffmembers = reportingstaffmembers;
+	}
+
+	public List<OfficeStaffMember> getReportingstaffmembers() {
+		return reportingstaffmembers;
+	}
+
+	public void setReportingstaffmembers(List<OfficeStaffMember> reportingstaffmembers) {
+		this.reportingstaffmembers = reportingstaffmembers;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [reportingstaffmembers=" + reportingstaffmembers + "]";
+	}
 }
